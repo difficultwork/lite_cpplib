dc5dbfeaf3d1796ca1af48d04f18737cacd5db5f
DifficultWork <39461955+DifficultWork@users.noreply.github.com>
2018-06-27 16:20:41 +0800

    Create lite_base.h

---
+++ lite_base.h
@@ -0,0 +1,14 @@
+
+
+#ifndef _LITE_BASE_H_
+#define _LITE_BASE_H_
+
+#if defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64)
+#define OS_WIN
+#elif defined(__linux)
+#define OS_LINUX
+#endif
+
+
+
+#endif
